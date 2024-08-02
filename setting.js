ca0c20089dd0f6a5b77f59bf228006eec8dd845e
Rftmnda22 <rafitamanda.r22@gmail.com>
2024-08-02 21:57:37 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,1 +1,16 @@
 console.log("JavaScript file is connected");
+console.log("JavaScript file is connected");
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
+});
