a61ea6f82fe38762429bf918c8ed245d6798cd97
Jein <jheinsara248@gmail.com>
2024-08-03 00:17:53 -0700

    Add files via upload

---
+++ HalamanAbout/setting.js
@@ -0,0 +1,15 @@
+console.log("JavaScript file is connected");
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
+});
